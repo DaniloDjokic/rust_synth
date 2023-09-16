f7ed2cc1db05419713e659510a2c369c9a242697
Danilo Djokic <danilo.djokic97@gmail.com>
2023-09-17 01:29:16 +0200

    Pull out next value

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,14 @@ fn main() {
 
     let sample_rate = config.sample_rate.0 as f32;
 
+    let _ = output_stream::run_stream(&device, sample_format, &config, get_next_value(sample_rate));
+}
+
+fn get_next_value(sample_rate: f32) -> Box<dyn FnMut() -> f32 + Send> {
     let mut sample_clock = 0f32;
-    let next_value = move || {
+
+    Box::new(move || {
         sample_clock = (sample_clock + 1.0) % sample_rate;
         (sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()
-    };
-
-    let _ = output_stream::run_stream(&device, sample_format, &config, next_value);
+    })
 }

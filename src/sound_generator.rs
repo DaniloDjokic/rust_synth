409cdc842de058a83cf6de423dcd99097726d159
Danilo <ddjokic@boylesports.com>
2023-09-18 17:15:04 +0200

    Add rdev lib to accept key press

--- src/sound_generator.rs
+++ src/sound_generator.rs
@@ -6,14 +6,15 @@ use note_listener::NoteListener;
 pub struct SampleGenerator {
     sample_rate: f32,
     clock: f32,
-    receiver: Receiver<Option<f32>>,
+    receiver: Receiver<f32>,
 }
 
 impl SampleGenerator {
     pub fn new(sample_rate: f32) -> Self {
         let (tx, rx) = mpsc::sync_channel(2);
 
-        NoteListener::start_listen(tx);
+        let listener = NoteListener::new(tx);
+        listener.start_listen();
 
         Self { sample_rate: sample_rate, clock: 0.0, receiver: rx }
     }
@@ -25,12 +26,7 @@ impl Iterator for SampleGenerator {
     fn next(&mut self) -> Option<Self::Item> {
         self.clock = (self.clock + 1.0) % self.sample_rate;
 
-        let hz  = self.receiver.recv().unwrap(); //error handling
-
-        let hz = match hz {
-            Some(h) => h,
-            None => 220.0
-        };
+        let hz = self.receiver.recv().unwrap();
 
         let next_sample = (self.clock * hz * 2.0 * std::f32::consts::PI / self.sample_rate).sin();
 
--- src/sound_generator/note_listener.rs
+++ src/sound_generator/note_listener.rs
@@ -1,30 +1,46 @@
 use std::thread::{self, JoinHandle};
-use std::sync::mpsc::SyncSender;
-use std::time::{Duration, SystemTime};
-
-pub struct NoteListener;
+use std::sync::mpsc::{self, Sender, SyncSender};
+use rdev::{listen, Event, EventType};
+pub struct NoteListener{
+    pressed_key: bool,
+    sender: SyncSender<f32>,
+}
 
 impl NoteListener {
-    pub fn start_listen(sender: SyncSender<Option<f32>>) -> JoinHandle<()> {
+    pub fn new(sender: SyncSender<f32>) -> Self {
+        Self { pressed_key: false, sender: sender }
+    }
+
+    pub fn start_listen(mut self) -> JoinHandle<()> {
         thread::spawn(move || {
-            let now = SystemTime::now();
-            let duration = Duration::new(1, 0);
+            let (tx, rx) = mpsc::channel();
+
+            NoteListener::foo(tx);
 
             loop {
-                match now.elapsed() {
-                    Ok(elapsed) => {
-                        if elapsed.as_secs() > duration.as_secs() {
-                            sender.send(Some(440.0)).unwrap(); //error handling
-                        }
-                        else {
-                            sender.send(None).unwrap();
-                        }
-                    },
-                    _ => panic!("Cannot elapse"),
+                let val = if self.pressed_key { 440.0 } else { 220.0 };
+                self.sender.send(val).unwrap();
+
+                if let Ok(press) = rx.try_recv() {
+                    self.pressed_key = press;
                 }
-                
             }
         })
     }
-}
 
+    fn foo(sender: Sender<bool>) {
+        let callback = move |event: Event| {
+            match event.event_type {
+                EventType::KeyPress(key) => sender.send(true).unwrap(),
+                EventType::KeyRelease(key) => sender.send(false).unwrap(),
+                _ => (),
+            }
+        };
+
+        thread::spawn(move || {
+            if let Err(e) = listen(callback) {
+                println!("{:?}", e);
+            }
+        });
+    }
+}

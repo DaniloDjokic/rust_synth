1354191561b726d8a37f3064881305ef5c098878
Danilo <ddjokic@boylesports.com>
2023-09-18 13:48:52 +0200

    Change thread to message passing

--- src/sound_generator.rs
+++ src/sound_generator.rs
@@ -1,21 +1,21 @@
 mod note_listener;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{self, Receiver};
 
 use note_listener::NoteListener;
 
 pub struct SampleGenerator {
     sample_rate: f32,
     clock: f32,
-    current_note: Arc<Mutex<f32>>,
+    receiver: Receiver<Option<f32>>,
 }
 
 impl SampleGenerator {
     pub fn new(sample_rate: f32) -> Self {
-        let current_note = Arc::new(Mutex::new(220.0));
+        let (tx, rx) = mpsc::sync_channel(2);
 
-        NoteListener::start_listen(Arc::clone(&current_note));
+        NoteListener::start_listen(tx);
 
-        Self { sample_rate: sample_rate, clock: 0.0, current_note: current_note}
+        Self { sample_rate: sample_rate, clock: 0.0, receiver: rx }
     }
 }
 
@@ -24,7 +24,13 @@ impl Iterator for SampleGenerator {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.clock = (self.clock + 1.0) % self.sample_rate;
-        let hz  = *self.current_note.lock().unwrap();
+
+        let hz  = self.receiver.recv().unwrap(); //error handling
+
+        let hz = match hz {
+            Some(h) => h,
+            None => 220.0
+        };
 
         let next_sample = (self.clock * hz * 2.0 * std::f32::consts::PI / self.sample_rate).sin();
 
--- src/sound_generator/note_listener.rs
+++ src/sound_generator/note_listener.rs
@@ -1,19 +1,30 @@
-use core::time;
-use std::{thread, sync::{Arc, Mutex}};
-#[allow(dead_code)]
-pub struct NoteListener {
-    current_note: Option<f32>
-}
-impl NoteListener {
-    pub fn start_listen(current_note: Arc<Mutex<f32>>) {
-        thread::spawn(move || {
-            loop {
-                thread::sleep(time::Duration::from_secs(1));
+use std::thread::{self, JoinHandle};
+use std::sync::mpsc::SyncSender;
+use std::time::{Duration, SystemTime};
 
-                let mut note = current_note.lock().unwrap();
-                *note = 440.0;
+pub struct NoteListener;
+
+impl NoteListener {
+    pub fn start_listen(sender: SyncSender<Option<f32>>) -> JoinHandle<()> {
+        thread::spawn(move || {
+            let now = SystemTime::now();
+            let duration = Duration::new(1, 0);
+
+            loop {
+                match now.elapsed() {
+                    Ok(elapsed) => {
+                        if elapsed.as_secs() > duration.as_secs() {
+                            sender.send(Some(440.0)).unwrap(); //error handling
+                        }
+                        else {
+                            sender.send(None).unwrap();
+                        }
+                    },
+                    _ => panic!("Cannot elapse"),
+                }
+                
             }
-        });
+        })
     }
 }
 
